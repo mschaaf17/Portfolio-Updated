d3671a4f5b3f0f7d9c2a74cf0f609225ed66b657
Maddy <madisonschaaf48@gmail.com>
2022-05-10 21:05:28 -0600

    added a button

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,24 @@ tl.fromTo(hero, 1, {height: "0%"}, {height: '80%', ease: Power2.easeInOut})
 
 
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut})
+
+
+
+//fun fact button information
+var funFactBtn = document.getElementById('funFactBtn')
+//use api to gather infomation or use api for albums
+var randomFacts = ["I love to play tennis.",
+"I enjoy driving around on my moped.",
+"I have lived in Utah my entire life.", 
+"I have two dogs."
+]
+//change fun fact after each click
+function changeFacts(e) {
+    funFactBtn= e.target
+    console.log('clicked')
+    var funFact = document.getElementById('funFact');
+    var randomTrivia= Math.floor(Math.random() * (randomFacts.length));
+    //you can do text.content or innerHTML
+    funFact.textContent= randomFacts[randomTrivia]     
+}  
+funFactBtn.addEventListener("click", changeFacts);
